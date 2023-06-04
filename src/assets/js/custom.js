0bc23126af7c0261edd56a4fff4e598024ef6d57
acigankov@inbox.ru <diiPs56uaY>
2023-06-04 20:30:21 +0300

    Доделал селект в форме

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -4,4 +4,7 @@ if(window.innerWidth < 767) {
         grabCursor: true,
     });
 }
-
+//selects
+const select = document.getElementById("a-select");
+NiceSelect.bind(select, {searchable: false, placeholder: 'Выбрать', searchtext: 'zoek', selectedtext: 'geselecteerd'});
+select.style.display = 'none'; //это нужно после инициалиазаци, иначе криво рисуется статчный селект
