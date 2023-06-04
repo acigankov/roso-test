b91b200622f916acf76262dafdf0c1e9e4fa0142
acigankov@inbox.ru <diiPs56uaY>
2023-06-04 22:35:50 +0300

    доделпл плашку с ссылками внизу страницы

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -5,10 +5,25 @@ if(window.innerWidth < 767) {
     });
 }
 //selects
-const select = document.getElementById("a-select");
+const select = document.getElementById('a-select');
 if (select) { //чтобы работало только на нужных страницах, т.е. только там, где есть такой элемент
     NiceSelect.bind(select, {
         searchable: false,
     });
     select.style.display = 'none'; //это нужно после инициалиазаци, иначе криво рисуется статчный селект
 }
+//contols-mini
+
+const toggler = document.getElementById('mini-toggler');
+const togglerMenu = document.getElementById('togglerMenu');
+
+//отслеживание клика по бургеру
+toggler.addEventListener('click', function (event) {
+    this.classList.toggle('open');
+
+    if(this.classList.contains('open')) {
+        togglerMenu.classList.add('open');
+    } else {
+        togglerMenu.classList.remove('open');
+    }
+});
